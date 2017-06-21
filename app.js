e16ffb309fc2d298c394f68cce8e50cdacddb1e4
niawkung <niawkung2@gmail.com>
2017-06-21 07:55:46 +0700

    fix

---
+++ app.js
@@ -0,0 +1,41 @@
+var $orders = $('#orders');
+var $name = $('#name');
+var $drink = $('#drink');
+var orderTemplate = $('#order-template').html();
+
+function addOrder(order) {
+    $orders.append(Mustache.render(orderTemplate, order));
+};
+
+$('#add-order').on('click', function() {
+
+    var order = {
+        name: $name.val(),
+        drink: $drink.val()
+    };
+
+    $.ajax({
+        type: 'POST',
+        url: 'http://rest.learncode.academy/api/learncode/friends',
+        data: order,
+        success: function(newOrder) {
+            addOrder(newOrder);
+        }
+    });
+});
+
+
+$.ajax({
+    type: 'GET',
+    url: 'http://rest.learncode.academy/api/learncode/friends',
+    success: function(data) {
+        $.each(data, function(i, order) {
+            if (order.id) {
+                addOrder(order);
+            }
+        });
+    },
+    error: function() {
+        alert('error loading orders');
+    }
+});
